efab7d517c75573f7d4f761a1d11e820f9819143
Christian Lehner <lehner.chri@gmail.com>
2015-01-16 16:06:10 +0100

    added header

--- freeShiftAlignment.js
+++ freeShiftAlignment.js
@@ -1,6 +1,12 @@
+/*
+Algorithms for the Social Web, Task 7
+Wanko, Lehner
+ */
+
+
 try {
   if(process.argv[2] === undefined || process.argv[3] === undefined) {
-    throw "\nNo or not all arguments given:"
+    throw "\nNo or not all arguments given."
   }
 }
 catch(err) {
@@ -8,18 +14,32 @@ catch(err) {
   process.exit(1); // failure
 }
 
+function createTwoDimensionalArray(length) {
+  var matrix = []
+  for(var i = 0; i < length.length; ++i) {
+    matrix.push([]);
+  }
+  return matrix;
+}
+
 var searchText = process.argv[2].toUpperCase();
 var searchKey = process.argv[3].toUpperCase();
 
 var gapPenalty = -1;
 
-var scoreMatrix = [];
+var scoreMatrix = createTwoDimensionalArray(searchText); // two dimensional array
 var scorePath = [];
 
 
 console.log("sentence: " + searchText);
 console.log("search: " + searchKey);
 
-for(var i = 0; i < searchText.length - 1; i++) {
+for(var i = 0; i < searchKey.length - 1; ++i) {
+  if(i != 0) {
+    scoreMatrix.push(gapPenalty * i);
+    scorePath.push("up");
+  }
+  else {
 
+  }
 }
