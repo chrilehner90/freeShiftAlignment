8d4429b8b2f4e20320abfed8e58734424ad655cb
MMTLukas <lwanko.mmt-m2014@fh-salzburg.ac.at>
2015-01-17 01:50:50 +0100

    check for all sentences and identifiers

--- freeShiftAlignment.js
+++ freeShiftAlignment.js
@@ -102,14 +102,78 @@ var identifiers = [
   "/videos/uploaded"
 ];
 
-try {
-  if (process.argv[2] === undefined || process.argv[3] === undefined) {
-    throw "\nNo or not all arguments given."
+var sentences = ["Find all friends who recently bought a pet and subsequently changed their relationship status.",
+  "Find all friends who have more friends than me.",
+  "How many friends of friends do I have?",
+  "Find people who like Das Kino or Mozartkino.",
+  "Find people who like The Shawshank Redemption.",
+  "Show me all companies which employ programmers near Salzburg!",
+  "Show me all people that I know who own more than a million euros!",
+  "Who of my friends are planning to attend a rock festival this year?",
+  "Who of my friends own a car in Salzburg?",
+  "Who of my friends play the same video games as I do?",
+  "Which friends will go out drinking today",
+  "Friends who likes drawing",
+  "Friends who play club football",
+  "Friends that are in university",
+  "Horny girls in my neighborhod",
+  "Get a list of people who like the movie „This is Spinal Tap“",
+  "Get a list of people who live in the same city as I, but do not have common friends.",
+  "Get a list of friends who did not wrote a facebook status since 1 month.",
+  "Get a friend who shares with me the greatest amount of the same like pages.",
+  "Get a list of friends who recently changed their relationship status.",
+  "Friends who likes FC Bayern Munich",
+  "Friends who play in a brass band",
+  "People who loves rib eye steaks medium-rare",
+  "People who live nearby and go fishing",
+  "Friends who ever won more than 10.000 euros",
+  "Mothers who like Bacon.",
+  "Married people who like Prostitutes.",
+  "Current employees of Google who like horses.",
+  "Single women who live nearby and who are interested in men and like Getting Drunk.",
+  "People who like Curry.",
+  "People who like pizza",
+  "Friends who use Instagram",
+  "Friends who have visited Bangkok",
+  "Friends of my Friends who live in Salzburg",
+  "Single women under 30 who live in Salzburg",
+  "Show me my friends pictures on mountains!",
+  "What restaurants do my friends in San Diego like?",
+  "Who of my friends works as an electrician?",
+  "Show me all pictures of my friends at the Nova Rock last year!",
+  "What schools have my friends from Villach been at?",
+  "Does anyone own a pogostick?",
+  "List all my friend's friends that are not in a relationship and live near Salzburg!",
+  "Who of my friends went to a music festival this year?",
+  "Show me all pictures of drunk friends!",
+  "Who is working as an engineer?",
+  "People who play guitar.",
+  "Bands which my friends have seen live.",
+  "Places my friends have visited last week.",
+  "Movies my friends saw and didn’t like.",
+  "Ex-girlfriends who are in a new relationship.",
+  "Friends of friends of my friends who study at the university of applied science",
+  "Restaurants nearby that I and at least one of my friends have been to",
+  "Photos from 1989 to 2014 I liked",
+  "Videos recorded by my friends in my hometown",
+  "Current employers of people who like racism"
+];
+
+var gapPenalty = -1;
+var correctAlignment = +1;
+
+for (var i = 0; i < sentences.length; i++) {
+  for (var j = 0; j < identifiers.length; j++) {
+    var sentence = sentences[i].toUpperCase().replace(/\/+/g, " ").replace(/(_)+/g, " ");
+    var identifier = identifiers[j].toUpperCase().replace(/\/+/g, " ").replace(/(_)+/g, " ");
+
+    var alignment = getAlignment(sentence, identifier);
+    if (alignment) {
+      console.log(alignment[0]);
+      console.log(alignment[1]);
+      console.log("================================================================================");
+    }
   }
-}
-catch (err) {
-  console.log(err + "\n\tUsage: node [scriptname] [sentence] [searchKey]\n");
-  process.exit(1); // failure
 }
 
 function createTwoDimensionalArray(length) {
@@ -120,87 +184,82 @@ function createTwoDimensionalArray(length) {
   return matrix;
 }
 
-var searchText = process.argv[2].toUpperCase();
-var searchKey = process.argv[3].toUpperCase();
+function getAlignment(searchText, searchKey) {
+  var scoreMatrix = createTwoDimensionalArray(searchKey);
+  var scorePath = createTwoDimensionalArray(searchKey);
 
-var gapPenalty = -1;
-var correctAlignment = +1;
-
-var scoreMatrix = createTwoDimensionalArray(searchKey); // two dimensional array
-var scorePath = createTwoDimensionalArray(searchKey);
-
-console.log("sentence: " + searchText);
-console.log("search: " + searchKey);
-
-for (var i = 0; i <= searchKey.length; ++i) { // rows
-  if (i > 0) { // leftmost column
-    scoreMatrix[i].push(gapPenalty * i);
-    scorePath[i].push("u"); // up
-  }
-  else { // top left corner
-    scoreMatrix[i].push(0);
-    scorePath[i].push("d"); // diagonal
-  }
-
-  for (var j = 1; j <= searchText.length; ++j) { // columns
-    if (i === 0) { // top row
-      scoreMatrix[i].push(0);
-      scorePath[i].push("l"); // left
+  for (var i = 0; i <= searchKey.length; ++i) { // rows
+    if (i > 0) { // leftmost column
+      scoreMatrix[i].push(gapPenalty * i);
+      scorePath[i].push("u"); // up
     }
-    else {
-      var values = 0;
+    else { // top left corner
+      scoreMatrix[i].push(0);
+      scorePath[i].push("d"); // diagonal
+    }
 
-      if (searchKey[i - 1] === searchText[j - 1]) {
-        values = [scoreMatrix[i - 1][j] + gapPenalty,
-          scoreMatrix[i][j - 1] + gapPenalty,
-          scoreMatrix[i - 1][j - 1] + correctAlignment];
+    for (var j = 1; j <= searchText.length; ++j) { // columns
+      if (i === 0) { // top row
+        scoreMatrix[i].push(0);
+        scorePath[i].push("l"); // left
       }
       else {
-        values = [scoreMatrix[i - 1][j] + gapPenalty,
-          scoreMatrix[i][j - 1] + gapPenalty,
-          scoreMatrix[i - 1][j - 1] - correctAlignment];
+        var values = 0;
+
+        if (searchKey[i - 1] === searchText[j - 1]) {
+          values = [scoreMatrix[i - 1][j] + gapPenalty,
+            scoreMatrix[i][j - 1] + gapPenalty,
+            scoreMatrix[i - 1][j - 1] + correctAlignment];
+        }
+        else {
+          values = [scoreMatrix[i - 1][j] + gapPenalty,
+            scoreMatrix[i][j - 1] + gapPenalty,
+            scoreMatrix[i - 1][j - 1] - correctAlignment];
+        }
+
+        var path = ["u", "l", "d"];
+        scoreMatrix[i][j] = Math.max.apply(Math, values);
+
+        var maxFromArray = Math.max.apply(Math, values);
+        scorePath[i][j] = path[values.indexOf(maxFromArray)];
       }
-
-      var path = ["u", "l", "d"];
-      scoreMatrix[i][j] = Math.max.apply(Math, values);
-
-      var maxFromArray = Math.max.apply(Math, values);
-      scorePath[i][j] = path[values.indexOf(maxFromArray)];
     }
   }
-}
 
-console.log(scoreMatrix);
-console.log(scorePath);
+  var maxValue = Math.max.apply(Math, scoreMatrix[scoreMatrix.length - 1]);
+  var maxValueIdx = scoreMatrix[scoreMatrix.length - 1].indexOf(maxValue);
 
-var maxValue = Math.max.apply(Math, scoreMatrix[scoreMatrix.length - 1]);
-var maxValueIdx = scoreMatrix[scoreMatrix.length - 1].indexOf(maxValue);
+  if (maxValue >= searchKey.length * (2 / 3)) {
+    var i = searchKey.length;
+    var j = maxValueIdx
 
-var i = searchKey.length;
-var j = maxValueIdx
+    var alignment = ["", ""];
+    for (var k = 0; k < searchText.length - j; ++k) {
+      alignment[0] += "-";
+    }
+    alignment[1] = searchText.slice(j);
+    alignment[2] = maxValue;
 
-var alignment = ["", ""];
-for (var k = 0; k < searchText.length - j; ++k) {
-  alignment[0] += "-";
-}
-alignment[1] = searchText.slice(j);
+    while (i > 0 || j > 0) {
+      if (scorePath[i][j] == "d") {
+        i--;
+        j--;
+        alignment[0] = searchKey[i] + alignment[0];
+        alignment[1] = searchText[j] + alignment[1];
+      } else if (scorePath[i][j] == "u") {
+        i--;
+        alignment[0] = searchKey[i] + alignment[0];
+        alignment[1] = "-" + alignment[1];
+      } else if (scorePath[i][j] == "l") {
+        j--;
+        alignment[0] = "-" + alignment[0];
+        alignment[1] = searchText[j] + alignment[1];
+      }
+    }
 
-while (i > 0 || j > 0) {
-  if (scorePath[i][j] == "d") {
-    i--;
-    j--;
-    alignment[0] = searchKey[i] + alignment[0];
-    alignment[1] = searchText[j] + alignment[1];
-  } else if (scorePath[i][j] == "u") {
-    i--;
-    alignment[0] = searchKey[i] + alignment[0];
-    alignment[1] = "-" + alignment[1];
-  } else if (scorePath[i][j] == "l") {
-    j--;
-    alignment[0] = "-" + alignment[0];
-    alignment[1] = searchText[j] + alignment[1];
+    return alignment;
+  }
+  else {
+    return null;
   }
 }
-
-console.log(alignment[0]);
-console.log(alignment[1]);
